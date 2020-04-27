01f7cb711f5eb9c0e93ac5ff20a4eebc9bebc8bc
cezary-ptaszek <cezaryptaszek@gmail.com>
2020-04-27 02:10:35 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-first';
+  title = 'To Do App';
+
+  task: string;
+  toDoList: Array<string> = [];
+  doneList: Array<string> = [];
+
+  addDoList() {
+    if (this.task !== undefined) {
+      this.toDoList.push(this.task);
+    }
+    this.task = '';
+  }
+
+  deleteDoList(index: number) {
+    this.toDoList.splice(index, 1);
+  }
+
+  addDoneList(index: number) {
+    this.doneList.push(this.toDoList[index]);
+    this.deleteDoList(index);
+  }
+
+  deleteDoneList(index: number) {
+    this.doneList.splice(index, 1);
+  }
 }
